bd6f896a49cba9caccea04657ec98d57bc4e6ff7
Hugo Mendez <hugo3011mendez@gmail.com>
2020-12-01 18:19:33 +0100

    - Locked vuelto a poner en False, FromBorderStyle establecido a FixedSingle para que no se pueda cambiar el tamaño de la ventana

--- Ejercicio2/Form1.Designer.cs
+++ Ejercicio2/Form1.Designer.cs
@@ -143,6 +143,7 @@
             this.Controls.Add(this.txtR);
             this.Controls.Add(this.btnSalir);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MaximizeBox = false;
